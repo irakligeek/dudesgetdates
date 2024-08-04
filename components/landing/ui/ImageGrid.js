dd100eba73d41279b6971675dd4cca7f96fa1c1d
Irakli <ikhakh@wineenthusiast.net>
2024-08-04 19:32:52 +0400

    Change hero styles

--- components/landing/ui/ImageGrid.js
+++ components/landing/ui/ImageGrid.js
@@ -3,10 +3,28 @@ import Image from "next/image";
 export default function ImageGrid() {
   return (
     <div className="container mx-auto p-6">
-      <div className="grid gap-4 grid-cols-2 md:grid-cols-3">
+      <div className="grid gap-4 grid-cols-2 md:grid-cols-2">
+      <div className="overflow-hidden space-y-4">
+          <Image
+            src="https://via.placeholder.com/600x900"
+            alt="Image 1"
+            // layout="responsive"
+            width={300}
+            height={600}
+            className="w-full rounded shadow-md"
+          />
+          <Image
+            src="https://via.placeholder.com/600x900"
+            alt="Image 1"
+            // layout="responsive"
+            width={300}
+            height={600}
+            className="w-full rounded shadow-md"
+          />
+        </div>
         <div className="overflow-hidden mt-24 space-y-4">
           <Image
-            src="https://via.placeholder.com/600x1000"
+            src="https://via.placeholder.com/600x900"
             alt="Image 1"
             // layout="responsive"
             width={300}
@@ -14,7 +32,7 @@ export default function ImageGrid() {
             className="w-full rounded shadow-md"
           />
           <Image
-            src="https://via.placeholder.com/600x1000"
+            src="https://via.placeholder.com/600x900"
             alt="Image 1"
             // layout="responsive"
             width={300}
@@ -23,7 +41,7 @@ export default function ImageGrid() {
           />
         </div>
 
-        <div className="overflow-hidden space-y-4">
+        {/* <div className="overflow-hidden mt-9 space-y-4">
           <Image
             src="https://via.placeholder.com/600x1000"
             alt="Image 1"
@@ -40,26 +58,7 @@ export default function ImageGrid() {
             height={600}
             className="w-full rounded shadow-md"
           />
-        </div>
-
-        <div className="overflow-hidden mt-9 space-y-4">
-          <Image
-            src="https://via.placeholder.com/600x1000"
-            alt="Image 1"
-            // layout="responsive"
-            width={300}
-            height={600}
-            className="w-full rounded shadow-md"
-          />
-          <Image
-            src="https://via.placeholder.com/600x1000"
-            alt="Image 1"
-            // layout="responsive"
-            width={300}
-            height={600}
-            className="w-full rounded shadow-md"
-          />
-        </div>
+        </div> */}
       </div>
     </div>
   );
